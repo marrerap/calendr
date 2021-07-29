692086d261bd84a85da239045ab3e3472db34474
marrerap <andrew.marrero@outlook.com>
2021-07-29 08:36:55 -0500

    final commit

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+// 3rd party modules
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
 
+// custom modules
+const checkAuth = require('./checkAuth')
+
+
+// routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const apiPatientsRouter = require('./routes/api/patients');
@@ -16,70 +23,29 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(
+    session({
+      secret: 'secret', // used to sign the cookie
+      resave: false, // update session even w/ no changes
+      saveUninitialized: true, // always create a session
+      cookie: {
+        secure: false, // true: only accept https req's
+        maxAge: 2592000, // time in seconds
+      }
+    })
+  );
+  
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/v1/patients', apiPatientsRouter);
-app.use('/api/v1/doctors', apiDoctorsRouter);
-app.use('/api/v1/appointments', apiAppointmentsRouter);
+app.use('/api/v1/patients', checkAuth, apiPatientsRouter);
+app.use('/api/v1/doctors', checkAuth, apiDoctorsRouter);
+app.use('/api/v1/appointments', checkAuth, apiAppointmentsRouter);
 
 
-
-// //   doctors post statement
-// app.post('/api/v1/doctors', (req, res) => {
-//     if (!req.body || !req.body.text) {
-//         // respond with an error
-//         res.status(422).json({
-//             error: "Must include Doctor Info"
-//         })
-//         return
-//     }
-//     // insert new todo into DB with text, and return newly created row
-//     db.result("INSERT INTO calendr.Doctor (text) VALUES ($1) RETURNING *", req.body.text)
-//         .then((results) => {
-//             // get the first result from the returned rows (should only be one anyway)
-//             const newDoctor = results.rows[0]
-//             // send that created item back to the client
-//             res.status(201).json(newDoctor)
-//         })
-// })
-
-
-
-
-// // patients and appointments post statement
-// app.post('/api/v1/todos', (req, res) => {
-//     if (!req.body || !req.body.text) {
-//         // respond with an error
-//         res.status(422).json({
-//             error: "must include todo text"
-//         })
-//         return
-//     }
-//     // insert new todo into DB with text, and return newly created row
-//     db.result("INSERT INTO todos (text) VALUES ($1) RETURNING *", req.body.text)
-//         .then((results) => {
-//             // get the first result from the returned rows (should only be one anyway)
-//             const createdTodo = results.rows[0]
-//             // send that created item back to the client
-//             res.status(201).json(createdTodo)
-//         })
-// })
-
-
-
-// // appointments get statement to grab all appointments
-// app.get('/api/v1/todos', (req, res) => {
-//     // Use any to fetch 0 or more results from database
-//     db.any("SELECT * FROM todos ORDER BY id")
-//         .then((todos) => {
-//             // respond with results
-//             res.json(todos)
-//         })
-// })
 
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,63 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcrypt')
+const db = require('../models')
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/register', function (req, res, next) {
+  if (!req.body || !req.body.email || !req.body.password) {
+    res.status(422).json({ error: "must include email and password" })
+    return
+  }
+  bcrypt.hash(req.body.password, 10)
+    .then((hash) => {
+      db.User.create({
+        email: req.body.email,
+        password: hash
+      })
+        .then((user) => {
+          res.status(210).json(user)
+        })
+    })
+
 });
+
+router.post("/login", (req, res) => {
+  if (!req.body || !req.body.email || !req.body.password) {
+    res.status(422).json({ error: "must include email and password" })
+  }
+
+  // find the user
+  db.User.findOne({
+    where: {
+      email: req.body.email
+    }
+  })
+    .then((user) => {
+      // check user password
+      bcrypt.compare(req.body.password, user.password)
+        .then((success) => {
+          if (success) {
+            // login in the user and display a successful login message
+            req.session.user = user;
+            res.json({
+              message: 'Successfully Logged In'
+            })
+          } else {
+            res.status(401).json({
+              error: "Incorrect Password"
+            })
+          }
+        })
+    })
+})
+
+router.get("/logout", (req, res) => {
+  req.session.user = null;
+  res.json({
+    message: "successfully logged out"
+  })
+})
 
 module.exports = router;
